172f38c7f07f880e6fe1a6bd1174cc0f53f86e29
theranjitkumar <web.ranjitkumar@gmail.com>
2019-04-03 13:10:02 +0530

    first commit with project arcitecture

---
+++ src/app/commons/services/http-interceptor.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse
+} from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    throw new Error("Method not implemented.");
+    return next.handle(req);
+  }
+
+}
---
+++ src/app/modules/private/private.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrivateComponent } from './private.component';
+
+@NgModule({
+  declarations: [PrivateComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PrivateModule { }
